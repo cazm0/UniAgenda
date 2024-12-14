7635e4e2947d4f42282d361b33c6c5abfae2ef3a
cazm0 <camilizeni@outlook.com>
2024-12-14 00:09:59 -0300

    Vite + Tailwind - inicios del diseño

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {spacing: {
+      '112': '28rem', // Tamaño personalizado (112 equivale a 28 rem)
+      '128': '32rem', // Tamaño personalizado (128 equivale a 32 rem)
+      '144': '36rem', // Tamaño personalizado (144 equivale a 36 rem)
+    }},
+  },
+  plugins: [],
+}
