a253242b2b0bd6d8d7579daf066043eaf4ad903b
cazm0 <camilizeni@outlook.com>
2024-12-14 00:28:43 -0300

    colores y formas mejoradas

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,27 @@ function App() {
   const numbers = Array.from({ length: 12 }, (_, i) => i + 1);
 
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <div className="relative w-128 h-128 bg-gray-500 rounded-full shadow-xl flex items-center justify-center">
-        {numbers.map((number, index) => {
-          const angle = (index * 30) - 60; // Divide el círculo en 12 partes (30° cada una)
-          const x = Math.cos((angle * Math.PI) / 180) * 200; // Ajusta el radio aquí
-          const y = Math.sin((angle * Math.PI) / 180) * 200;
+    <div className="flex items-center justify-center h-screen bg-beige">
+        <div className="relative w-144 h-144 bg-red rounded-full shadow-xl flex items-center justify-center">
+        <div className="absolute w-128 h-128 bg-beige rounded-full"></div>
+          {numbers.map((number, index) => {
+            const angle = (index * 30) - 60; // Divide el círculo en 12 partes (30° cada una)
+            const x = Math.cos((angle * Math.PI) / 180) * 273; // Ajusta el radio aquí
+            const y = Math.sin((angle * Math.PI) / 180) * 273;
 
-          return (
-            <div
-              key={number}
-              className="absolute text-white font-bold text-lg"
-              style={{
-                transform: `translate(${x}px, ${y}px)`,
-              }}
-            >
-              {number}
-            </div>
-          );
-        })}
-      </div>
+            return (
+              <div
+                key={number}
+                className="absolute text-beige font-bold text-lg"
+                style={{
+                  transform: `translate(${x}px, ${y}px)`,
+                }}
+              >
+                {number}
+              </div>
+            );
+          })}
+        </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ export default {
       '112': '28rem', // Tamaño personalizado (112 equivale a 28 rem)
       '128': '32rem', // Tamaño personalizado (128 equivale a 32 rem)
       '144': '36rem', // Tamaño personalizado (144 equivale a 36 rem)
-    }},
+    },
+    colors: {
+      beige: '#F8EDE3',
+      red: '#C5705D'
+    },},
   },
   plugins: [],
 }
